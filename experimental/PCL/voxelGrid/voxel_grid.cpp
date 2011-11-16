8fc243e6d2112a2b702430ffc48e637b92fab7aa
Francis Engelmann <francis.engelmann@gmail.com>
2011-11-16 12:14:05 +0100

    added PCL voxelGrid example

---
+++ experimental/PCL/voxelGrid/voxel_grid.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
+int
+main (int argc, char** argv)
+{
+  sensor_msgs::PointCloud2::Ptr cloud (new sensor_msgs::PointCloud2 ());
+  sensor_msgs::PointCloud2::Ptr cloud_filtered (new sensor_msgs::PointCloud2 ());
+
+  // Fill in the cloud data
+  pcl::PCDReader reader;
+  // Replace the path below with the path where you saved your file
+  reader.read ("../../../pointclouds/bearHigh.pcd", *cloud); // Remember to download the file first!
+
+  std::cerr << "PointCloud before filtering: " << cloud->width * cloud->height 
+       << " data points (" << pcl::getFieldsList (*cloud) << ").";
+
+  // Create the filtering object
+  pcl::VoxelGrid<sensor_msgs::PointCloud2> sor;
+  sor.setInputCloud (cloud);
+  sor.setLeafSize (0.1f, 0.1f, 0.1f);
+  sor.filter (*cloud_filtered);
+
+  std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height 
+       << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
+
+  pcl::PCDWriter writer;
+  writer.write ("bearHigh_downsampled.pcd", *cloud_filtered, 
+         Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), false);
+
+  return (0);
+}
