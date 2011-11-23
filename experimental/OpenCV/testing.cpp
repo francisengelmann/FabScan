ac878dbfbc853f3e5e856be23323e69d3d8e1cb3
Francis Engelmann <francis.engelmann@gmail.com>
2011-11-23 10:11:36 +0100

    added OpenCV testing exmaple, not yet working

---
+++ experimental/OpenCV/testing.cpp
@@ -0,0 +1,20 @@
+//#include "cv.h"
+#include <OpenCV/OpenCV.h>
+//#include <highgui.h>
+
+int main ( int argc, char **argv )
+{
+  cvNamedWindow( "My Window", 1 );
+  IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
+  CvFont font;
+  double hScale = 1.0;
+  double vScale = 1.0;
+  int lineWidth = 1;
+  cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC,
+              hScale, vScale, 0, lineWidth );
+  cvPutText( img, "Hello World!", cvPoint( 200, 400 ), &font,
+             cvScalar( 255, 255, 0 ) );
+  cvShowImage( "My Window", img );
+  cvWaitKey();
+  return 0;
+}
