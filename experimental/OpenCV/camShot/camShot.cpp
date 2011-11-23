8bddfe22af960f590e148c9b282c27c36e7ed52b
Francis Engelmann <francis.engelmann@gmail.com>
2011-11-23 11:05:15 +0100

    added opencv example displaying a window containing a picture from an atached webcam

---
+++ experimental/OpenCV/camShot/camShot.cpp
@@ -0,0 +1,20 @@
+#ifdef __APPLE__
+#include <OpenCV/OpenCV.h>
+#endif
+
+#ifdef __linux__
+#include "cv.h"
+#include "highgui.h"
+#endif
+
+//#include <highgui.h>
+
+int main ( int argc, char **argv )
+{
+  CvCapture* capture = cvCreateCameraCapture(0);
+  IplImage* pframe = cvQueryFrame(capture);
+  cvShowImage("Cam Shot", pframe);
+  cvWaitKey();
+  //cvSaveImage("camShot.png",pframe);
+  return 0;
+}
