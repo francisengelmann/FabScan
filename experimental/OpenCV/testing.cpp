212c24e242b1672f99c9f394d71fb62d0ae98605
Francis Engelmann (root@atom) <francis.engelmann@rwth-aachen.de>
2011-11-23 11:11:10 +0100

    fixed CMakeLists for linux

--- experimental/OpenCV/testing.cpp
+++ experimental/OpenCV/testing.cpp
@@ -1,20 +1,18 @@
-//#include "cv.h"
-#include <OpenCV/OpenCV.h>
+#ifdef __APPLE__
+  #include <OpenCV/OpenCV.h>
+#endif
+
+#ifdef __linux__
+  #include "cv.h"
+  #include "highgui.h"
+#endif
+
 //#include <highgui.h>
 
 int main ( int argc, char **argv )
 {
-  cvNamedWindow( "My Window", 1 );
-  IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
-  CvFont font;
-  double hScale = 1.0;
-  double vScale = 1.0;
-  int lineWidth = 1;
-  cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC,
-              hScale, vScale, 0, lineWidth );
-  cvPutText( img, "Hello World!", cvPoint( 200, 400 ), &font,
-             cvScalar( 255, 255, 0 ) );
-  cvShowImage( "My Window", img );
-  cvWaitKey();
+  CvCapture* capture = cvCreateCameraCapture(0);
+  IplImage* pframe = cvQueryFrame(capture);
+  cvSaveImage("camShot.png",pframe);
   return 0;
 }
