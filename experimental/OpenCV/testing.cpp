08809b66d7a195f4d0d3f798f0570fa1b5241825
Francis Engelmann <francis.engelmann@gmail.com>
2011-11-23 10:47:57 +0100

    more testing with opencv

--- experimental/OpenCV/testing.cpp
+++ experimental/OpenCV/testing.cpp
@@ -7,12 +7,19 @@
   #include "highgui.h"
 #endif
 
-//#include <highgui.h>
-
 int main ( int argc, char **argv )
 {
-  CvCapture* capture = cvCreateCameraCapture(0);
-  IplImage* pframe = cvQueryFrame(capture);
-  cvSaveImage("camShot.png",pframe);
+  cvNamedWindow( "My Window", 1 );
+  IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
+  CvFont font;
+  double hScale = 1.0;
+  double vScale = 1.0;
+  int lineWidth = 1;
+  cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC,
+              hScale, vScale, 0, lineWidth );
+  cvPutText( img, "Hello World!", cvPoint( 200, 400 ), &font,
+             cvScalar( 255, 255, 0 ) );
+  cvShowImage( "My Window", img );
+  cvWaitKey();
   return 0;
 }
