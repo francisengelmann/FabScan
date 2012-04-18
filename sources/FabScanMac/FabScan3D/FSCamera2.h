edacf4b2acf80019f54bb2619611137381ebffdf
Francis Engelmann <francis.engelmann@gmail.com>
2012-04-18 09:58:42 +0200

    added point cloud from a white coffee cup

---
+++ sources/FabScanMac/FabScan3D/FSCamera2.h
@@ -0,0 +1,13 @@
+//
+//  Camera2.h
+//  FabScan
+//
+//  Created by Francis Engelmann on 4/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Camera2 : NSObject
+
+@end
---
+++ sources/FabScanMac/FabScan3D/FSCamera2.mm
@@ -0,0 +1,23 @@
+//
+//  Camera2.m
+//  FabScan
+//
+//  Created by Francis Engelmann on 4/8/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Camera2.h"
+
+@implementation Camera2
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
